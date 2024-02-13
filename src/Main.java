12e866acbc1497f6b502392da620cc5c0e33fad6
MichalKlemm <Klemmmichal@lgmail.com>
2024-02-13 19:20:04 +0100

    Ukázka řazení seznamu
    Základní řazení s využitím Comparator.comparing(...).
    
    ukázka použití .reverse().

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import cz.engeto.ja.Computer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +18,20 @@ public class Main {
            System.out.println(computer.getDescription()+" "+ computer.getYearOfProduction()
            +" "+ computer.getPrice());
        }
+
+        computers.sort(Comparator.comparing(Computer::getYearOfProduction));
+
+       // Obecnější pro jakoukoli kolekci: Collection.sort(computers, Comparator.comparing(Computer::getPrice));
+
+       System.out.println("\nSeřazeno podle roku výroby:\n");
+       computers.forEach(System.out::println);
+
+        computers.sort(Comparator.comparing(Computer::getPrice).reversed());
+
+        // v základu se řadí od nejnižší hodnoty -> reverse to otočí od nejvyšší
+
+        System.out.println("\nSeřazeno od nejvyšší ceny:\n");
+        computers.forEach(System.out::println);
+
     }
 }
